ee3cc45f969c9ab661bb198ab4379247de7faebd
wuh3 <wuh3@rose-hulman.edu>
2019-05-09 08:47:15 -0400

    Added my name

--- src/m2_laptop_code.py
+++ src/m2_laptop_code.py
@@ -3,7 +3,7 @@
   Displays the Graphical User Interface (GUI) and communicates with the robot.
 
   Authors:  Your professors (for the framework)
-    and PUT_YOUR_NAME_HERE.
+    and Haozhe Wu.
   Spring term, 2018-2019.
 """
 # TODO 1:  Put your name in the above.
